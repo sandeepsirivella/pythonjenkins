13f1059fd9268091f30292bcdd26d6e03584e15b
sandeepsirivella <sirivellasandeep@gmail.com>
2025-08-29 11:17:22 +0000

    my_python1.py modified

--- my_python1.py
+++ my_python1.py
@@ -15,7 +15,7 @@ def main():
     try:
         num1 = float(input("Enter first number: "))
         num2 = float(input("Enter second number: "))
-d       result = num1 + num2
+       result = num1 + num2
         print(f"The sum of {num1} and {num2} is: {result}")
     except ValueError:
         print("Please enter valid numbers!")
